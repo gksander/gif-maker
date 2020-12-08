fa82e3c3bbe8373c8f04190767dcc7a26ea60c90
Grant Sander <gksander93@gmail.com>
2020-12-08 16:07:24 -0600

    Rename file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./service-worker";
 import "./styles/index.css";
 
 ReactDOM.render(
